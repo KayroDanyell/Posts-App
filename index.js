b9505249ad588dd488f3610f36df8964df6e3cd3
kayro ghost <kayrodanyell@gmail.com>
2021-01-06 19:57:25 -0300

    commitando indexjs

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const PORT = 5000
+const path = require('path')
+const apiRoute = require('./routes/api')
+
+const app = express()
+
+app.use('/api', apiRoute )
+app.use('/',express.static(path.join(__dirname, 'public')))
+
+
+
+app.listen(PORT, ()=>{
+    console.log("Server rodano na porta", PORT)
+})
+
