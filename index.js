31efdfe513fdd689fcc393daf4be88cb98008d64
kayro ghost <kayrodanyell@gmail.com>
2021-01-07 11:05:08 -0300

    mongoose connection

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const PORT = 5000
 const path = require('path')
 const apiRoute = require('./routes/api')
+const mongoose = require("mongoose")
+
+mongoose.connect('mongodb://localhost/posts', { useNewUrlParser: true, useUnifiedTopology: true }, (error,db) =>{
+    console.log(error);
+    console.log(db)
+})
 
 const app = express()
 
